913c914b7bfa5a4ec5c37266d2088a441d198261
este6an13 <57577966+este6an13@users.noreply.github.com>
2022-04-21 00:55:49 -0500

    db implementation

--- images/images_ms.py
+++ images/images_ms.py
@@ -1,9 +1,27 @@
-
 from flask import Flask, request
 import base64
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, migrate
+import os
 
 # create the Flask app
 app = Flask(__name__)
+
+# adding configuration for using a sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+ 
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
+ 
+# Settings for migrations
+migrate = Migrate(app, db)
+ 
+# Models
+class Image(db.Model):
+    # database table.
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, unique=False, nullable=False)
+ 
 
 @app.route('/image', methods=['GET', 'DELETE', 'POST'])
 def image():
@@ -15,29 +33,34 @@ def image():
         return b64_string
     elif request.method == 'DELETE':       
         id = data['id']
-        # call images_data_db to remove image data with id 'id'
-        # call image storage and delete image file with id 'id'
+        data = Image.query.get(id)
+        db.session.delete(data)
+        db.session.commit()
+        os.remove('image_storage\{0}.jpg'.format(id)) # temporarily in local
         print('image with id \'{0}\' has been deleted'.format(id))
         return {'Id':id, 'Result':'DELETED'}
     elif request.method == 'POST':
         b64 = data['b64']
-        id = data['id'] # generar en backend con alguna función ...
-        new_image = open('image_storage\{0}.jpg'.format(id), 'wb')
-        new_image.write(base64.b64decode((b64)))
-        new_image.close()
-        return {'Id':id, 'Result':'POSTED'}
+        user_id = data['user_id']
+        image = Image(user_id=user_id) # id is created automatically, it will be the name of the image file
+        db.session.add(image)
+        db.session.commit()
+        image_file = open('image_storage\{0}.jpg'.format(image.id), 'wb')
+        image_file.write(base64.b64decode((b64)))
+        image_file.close()
+        return {'Id':image.id, 'Result':'POSTED'}
 
 
 @app.route('/images', methods=['GET'])
 def images():
     data = request.get_json()
     user_id = data['user_id']
-    # call images_data_db to get ids of images belonging to user with user id 'user_id'
-    ids = ['dog', 'tree', 'bridge']
+    # ids = ['dog', 'tree', 'bridge'] # now images will be named by their id (int)
+    images = db.session.query(Image).filter(Image.user_id == user_id)
     b64_string_dict = {}
-    for id in ids:
-        with open('image_storage\{0}.jpg'.format(id), 'rb') as img_file:
-            b64_string_dict[id] = base64.b64encode(img_file.read()).decode()
+    for img in images:
+        with open('image_storage\{0}.jpg'.format(img.id), 'rb') as img_file:
+            b64_string_dict[img.id] = base64.b64encode(img_file.read()).decode()
     return b64_string_dict
 
 if __name__ == '__main__':
