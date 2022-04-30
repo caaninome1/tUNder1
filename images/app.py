7e968b4b2b4e09892aef3bda70c4d06d4d5f75ed
este6an13 <57577966+este6an13@users.noreply.github.com>
2022-04-29 20:50:41 -0500

    Update and rename images_ms.py to app.py

--- images/app.py
+++ images/app.py
@@ -1,20 +1,24 @@
-from flask import Flask, request
 import base64
+import os
+
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
-import os
+
 
 # create the Flask app
 app = Flask(__name__)
 
 # adding configuration for using a sqlite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://tUNder:2022@172.17.0.2/tUNder_images_db'
  
 # Creating an SQLAlchemy instance
 db = SQLAlchemy(app)
  
 # Settings for migrations
 migrate = Migrate(app, db)
+
+dirname = os.path.dirname(__file__)
  
 # Models
 class Image(db.Model):
@@ -28,7 +32,7 @@ def image():
     data = request.get_json()
     if request.method == 'GET':
         id = data['id']
-        with open('image_storage\{0}.jpg'.format(id), 'rb') as img_file:
+        with open('image_storage/{0}.jpg'.format(id), 'rb') as img_file:
             b64_string = base64.b64encode(img_file.read())
         return b64_string
     elif request.method == 'DELETE':       
@@ -36,7 +40,7 @@ def image():
         data = Image.query.get(id)
         db.session.delete(data)
         db.session.commit()
-        os.remove('image_storage\{0}.jpg'.format(id)) # temporarily in local
+        os.remove('image_storage/{0}.jpg'.format(id)) # temporarily in local
         print('image with id \'{0}\' has been deleted'.format(id))
         return {'Id':id, 'Result':'DELETED'}
     elif request.method == 'POST':
@@ -45,7 +49,7 @@ def image():
         image = Image(user_id=user_id) # id is created automatically, it will be the name of the image file
         db.session.add(image)
         db.session.commit()
-        image_file = open('image_storage\{0}.jpg'.format(image.id), 'wb')
+        image_file = open(os.path.join(dirname, 'image_storage/{0}.jpg'.format(image.id)), 'wb') # use backslash if you are in Windows
         image_file.write(base64.b64decode((b64)))
         image_file.close()
         return {'Id':image.id, 'Result':'POSTED'}
@@ -59,10 +63,10 @@ def images():
     images = db.session.query(Image).filter(Image.user_id == user_id)
     b64_string_dict = {}
     for img in images:
-        with open('image_storage\{0}.jpg'.format(img.id), 'rb') as img_file:
+        with open('image_storage/{0}.jpg'.format(img.id), 'rb') as img_file:
             b64_string_dict[img.id] = base64.b64encode(img_file.read()).decode()
     return b64_string_dict
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
