94d6970717e423ce08909cae07c547b8c52c9aae
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2022-06-14 10:21:25 -0500

    udated views

--- tunder_wa/src/components/Home.vue
+++ tunder_wa/src/components/Home.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="MatchesPaneDiv" >
+  <div class="row">
+    <div class="col col-5 col-md-4">
+      <button class="btn btn-primary" v-on:click="logout()">Logout</button>
+    </div>
+    <div class="col col-5 col-md-4">
+      <button class="btn btn-primary" v-on:click="goComic()">Comics</button>
+    </div>
+  </div>
+  <div id="MatchesPaneDiv">
     <MatchesPane />
   </div>
-  <br>
+  <br />
   <div id="InteractionPaneDiv">
     <InteractionPane />
   </div>
 </template>
 
 <script>
-import MatchesPane from './MatchesPane.vue'
-import InteractionPane from './InteractionPane.vue'
+import MatchesPane from "./MatchesPane.vue";
+import InteractionPane from "./InteractionPane.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     MatchesPane,
-    InteractionPane
-  }
-}
+    InteractionPane,
+  },
+  methods: {
+    logout() {
+      localStorage.clear();
+      this.$router.replace("/login");
+    },
+    goComic() {
+      this.$router.replace("/comics");
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-
-
-</style>
+<style scoped></style>
--- tunder_wa/src/components/LoginPane.vue
+++ tunder_wa/src/components/LoginPane.vue
@@ -1,115 +1,114 @@
 <template>
   <div class="login">
     <center>
-      <h1 class="title" style="color:green">Bienvenido a Tunder</h1>
-      <br>
-      
-      <form v-on:submit.prevent="processLogin" action class="form">
-          <div class="form-group col-10 col-md-4">
-            <input
-              class="form-control"
-              placeholder="Usuario"
-              type="email"
-              v-model="user.username"
-              required
-            />
-          </div>
-          <br>
-          <div class="form-group col-10 col-md-4">
-            <input
-              class="form-control"
-              type="password"
-              v-model="user.password"
-              placeholder="Contraseña"
-              required
-            />
-          </div>
-          <br>
-          <button type="submit" class="btn btn-primary">Ingresar</button>
+      <h1 class="title" style="color: green">Bienvenido a Tunder</h1>
+      <br />
 
+      <form v-on:submit.prevent="processLogin" action class="form">
+        <div class="form-group col-10 col-md-4">
+          <input
+            class="form-control"
+            placeholder="Usuario"
+            type="email"
+            v-model="user.username"
+            required
+          />
+        </div>
+        <br />
+        <div class="form-group col-10 col-md-4">
+          <input
+            class="form-control"
+            type="password"
+            v-model="user.password"
+            placeholder="Contraseña"
+            required
+          />
+        </div>
+        <br />
+        <button type="submit" class="btn btn-primary">Ingresar</button>
+        <button type="button" class="btn btn-primary" v-on:click="goRegist()">
+          Registro
+        </button>
       </form>
     </center>
   </div>
 </template>
 <script>
+import gql from "graphql-tag";
+export default {
+  name: "LoginPane",
 
-  import gql from "graphql-tag";
-  export default {
-    name: "LoginPane",
-  
-    data: function() {
-      return {
-        user: {
-          username: "",
-          password: "",
-        },
-        showDismissibleAlert: false
-      };
-    },
-    methods: {
-      renewtoken( ){
-        this.$apollo.mutate({
+  data: function () {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+      showDismissibleAlert: false,
+    };
+  },
+  methods: {
+    renewtoken() {
+      this.$apollo
+        .mutate({
           mutation: gql`
-              mutation ($token: String!) {
-                  refreshToken(token: $token) {
-                      token,
-                  }
+            mutation ($token: String!) {
+              refreshToken(token: $token) {
+                token
               }
+            }
           `,
-          
+
           variables: {
-              token : localStorage.token 
+            token: localStorage.token,
           },
         })
-        .then(({data}) => {
+        .then(({ data }) => {
           console.log(data.refreshToken.token);
           setTimeout(this.renewtoken, 3600000);
-
         })
         .catch((error) => {
-          console.log ( error )
+          console.log(error);
         });
-       
-      },
-      processLogin: function() {
-        this.$apollo.mutate({
+    },
+    goRegist() {
+      this.$router.replace("/signup");
+    },
+    processLogin: function () {
+      this.$apollo
+        .mutate({
           mutation: gql`
-              mutation ($user: NewUser!) {
-                  login(user: $user) {
-                      token,
-                      userID
-                  }
+            mutation ($user: NewUser!) {
+              login(user: $user) {
+                token
+                userID
               }
+            }
           `,
-          
+
           variables: {
-              user: {
-                      email:  this.user.username,
-                      password: this.user.password, // profileId as string
-              },
+            user: {
+              email: this.user.username,
+              password: this.user.password, // profileId as string
+            },
           },
         })
-        .then(({data}) => {
-          try{
-            localStorage.token = data.login.token ;
-            localStorage.userId = data.login.userID ;
-            this.$store.state.userId = data.login.userID ;
+        .then(({ data }) => {
+          try {
+            localStorage.token = data.login.token;
+            localStorage.userId = data.login.userID;
+            this.$store.state.userId = data.login.userID;
             setTimeout(this.renewtoken, 3600000);
             this.$router.push({ name: "home" });
-          }catch{
+          } catch {
             alert("Usuario o contraseña incorrecto");
           }
-         
         })
         .catch((error) => {
           alert("Usuario o contraseña incorrecto");
           console.log(error);
         });
-       
-       
-      }
     },
-   
-  }
+  },
+};
 </script>
-
