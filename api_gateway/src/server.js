db64d7b07f018085dbc512aecb2e39613fca50ff
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2022-06-17 20:44:45 -0500

    Kubernetes deploy

--- api_gateway/src/server.js
+++ api_gateway/src/server.js
@@ -5,7 +5,8 @@ module.exports = {
   //profile_ms_url: "https://profile-ms-y6bebo7xta-uc.a.run.app",
   //interaction_ms_url: "https://interactions-ms-y6bebo7xta-uc.a.run.app",
 
-  user_ms_url: "https://user-ms-5fz6wzxo5a-uc.a.run.app",
+  //user_ms_url: "https://user-ms-5fz6wzxo5a-uc.a.run.app",
+  user_ms_url: "http://34.70.192.148:8080",
   chat_ms_url: "https://chat-ms-5fz6wzxo5a-uc.a.run.app",
   images_ms_url: "https://images-ms-5fz6wzxo5a-uc.a.run.app",
   profile_ms_url: "https://profile-ms-5fz6wzxo5a-uc.a.run.app",
