c9790edad85f6ed4e0770beb04777186bcf49cac
caaninome1 <caaninome@unal.edu.co>
2022-11-30 01:32:28 -0500

    docker api

--- api_gateway/src/server.js
+++ api_gateway/src/server.js
@@ -6,9 +6,9 @@ module.exports = {
   //interaction_ms_url: "https://interactions-ms-y6bebo7xta-uc.a.run.app",
 
   //user_ms_url: "https://user-ms-5fz6wzxo5a-uc.a.run.app",
-  user_ms_url: "http://34.70.192.148:8080",
-  chat_ms_url: "https://chat-ms-5fz6wzxo5a-uc.a.run.app",
-  images_ms_url: "https://images-ms-5fz6wzxo5a-uc.a.run.app",
-  profile_ms_url: "https://profile-ms-5fz6wzxo5a-uc.a.run.app",
-  interaction_ms_url: "https://interaction-ms-5fz6wzxo5a-uc.a.run.app",
+  user_ms_url: "https://tunder-user-ms-1-d3il2bbdea-uc.a.run.app",
+  //chat_ms_url: "https://chat-ms-5fz6wzxo5a-uc.a.run.app",
+  images_ms_url: "https://image-ms.up.railway.app",
+  profile_ms_url: "https://profile-production-76c7.up.railway.app",
+  interaction_ms_url: "https://interaction-ms-d3il2bbdea-uc.a.run.app",
 };
